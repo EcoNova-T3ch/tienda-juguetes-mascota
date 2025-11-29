7dac1cd80b26dcd493415f9e463217b86f36fcab
Patrik Cruz <patrikarnoldcruzpallqui@gmail.com>
2025-11-29 04:08:57 +0000

    Taller 5 - IHC

--- menu_toggle.js
+++ menu_toggle.js
@@ -1,14 +1,14 @@
 // Selecciona el botón de hamburguesa
-const menuToggle = document.querySelector(".menu-toggle");
+const btn = document.querySelector(".menu-toggle");
 
 // Selecciona el menú horizontal
-const menuHorizontal = document.querySelector(".Menu-Horizontal");
+const menu = document.querySelector(".Menu-Horizontal");
 
 // Al hacer clic en el botón de hamburguesa
-menuToggle.addEventListener("click", () => {
+btn.addEventListener("click", () => {
   // Alterna (agrega o quita) la clase "active" en el menú
   // Esto hace que se muestre o se oculte
-  menuHorizontal.classList.toggle("active");
+  menu.classList.toggle("active");
 
   // (opcional) También puedes alternar una clase en el botón si quieres cambiar su apariencia
   // btn.classList.toggle("open");
